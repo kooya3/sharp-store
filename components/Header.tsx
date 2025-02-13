2161745be5210e49e73b8b2cc82dd76deaddf0d5
Elyees Tatua <elyeestatua@gmail.com>
2025-02-13 18:29:12 +0300

    WIP : creating the search functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
+import Form from "next/form";
 
 function Header() {
     const { user } = useUser();
@@ -17,6 +18,31 @@ function Header() {
             >
                 Sharp 🛒
             </Link>
+
+            <Form
+                action="/search"
+                className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
+            >
+                <input
+                    type="text"
+                    name="query"
+                    placeholder="Search for products"
+                    className="
+              bg-gray-100
+                text-gray-800
+                px-4
+                py-2
+                rounded
+                focus:outline-none
+                focus:ring-2
+                focus:ring-blue-500
+                focus:ring-opacity-50
+                border
+                w-full
+                max-w-4xl
+            "
+                />
+            </Form>
         </div>
     </header>
 }
