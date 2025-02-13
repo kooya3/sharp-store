c682673b7104186a2e95e7d0782a77f651071c6e
Elyees Tatua <elyeestatua@gmail.com>
2025-02-13 16:09:58 +0300

    WIP : setting up project

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +27,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body
-        >
+      <body>
+        <main>
+        <Header />
         {children}
+        </main>    
       </body>
     </html>
     </ClerkProvider>
