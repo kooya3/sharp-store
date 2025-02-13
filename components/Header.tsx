929fb1a03aca8d0bf2fab694419166049537f5e0
Elyees Tatua <elyeestatua@gmail.com>
2025-02-13 18:34:37 +0300

    WIP : creating the search functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,13 @@
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import Form from "next/form";
+import { TrolleyIcon } from "@sanity/icons";
 
 function Header() {
     const { user } = useUser();
+    
 
-    console.log(user);
+
 
     return <header className="flex flex-wrap justify-between items-center px-4 py-2">
         {/* Top Row */}
@@ -43,7 +45,22 @@ function Header() {
             "
                 />
             </Form>
-        </div>
+
+            <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
+                <Link
+                    href="/basket"
+                    className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    <TrolleyIcon className="w-6 h-6" />
+
+                    <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                    
+                    </span>
+
+                    <span>My Basket</span>
+                </Link>
+                </div>
+            </div>
     </header>
 }
 
