3bbb5c443f91ae5d9a5494d61d86519ed51e9821
Elyees Tatua <elyeestatua@gmail.com>
2025-02-13 18:24:04 +0300

    WIP : working on the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client'
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+
+function Header() {
+    const { user } = useUser();
+
+    console.log(user);
+
+    return <header className="flex flex-wrap justify-between items-center px-4 py-2">
+        {/* Top Row */}
+        <div>
+            <Link
+                href="/"
+                className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+            >
+                Sharp 🛒
+            </Link>
+        </div>
+    </header>
 }
 
 export default Header
